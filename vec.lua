c875532aa8bc1f4fa77a53f86c632741547dca3e
Christopher Moore <christopher.e.moore@gmail.com>
2016-06-01 12:59:33 +0300

    added more to the global namespace

--- vec.lua
+++ vec.lua
@@ -1,4 +1,7 @@
--- TODO don't use this file, and keep classes local
+-- for global namespace 
 vec2 = require 'vec.vec2'
 vec3 = require 'vec.vec3'
 vec4 = require 'vec.vec4'
+quat = require 'vec.quat'
+box2 = require 'vec.box2'
+box3 = require 'vec.box3'
